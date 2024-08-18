e9ea794bfebccf0cea737bccb3b826140a4fbd3a
Владислав Муртазин <superbia227@gmail.com>
2024-08-18 18:46:52 +0300

    Fix build type error in src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app';
 import './shared/config/styles/globals.scss';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
