9a1830ee0692a9c7206bce720f54707d650bf689
Владислав Муртазин <superbia227@gmail.com>
2024-08-24 21:50:41 +0300

    Added debounce time for better mouse collision behavior

--- src/shared/lib/hooks/useHeroAnimation.ts
+++ src/shared/lib/hooks/useHeroAnimation.ts
@@ -1,16 +1,18 @@
-import { MutableRefObject, RefObject, useEffect } from 'react';
+import { MutableRefObject, RefObject, useEffect, useRef } from 'react';
 import { Hero } from '../../../entities/game';
 import { Position } from '../../types/globals.ts';
 import { colors } from '../../config/styles/variables.ts';
 
-const mouseProximityRadius: number = 30;
-const debounceTime: number = 50;
+const mouseProximityRadius: number = 20;
+const debounceTime: number = 100;
+
 const useHeroAnimation = (
   canvasRef: RefObject<HTMLCanvasElement>,
   heroes: ReadonlyArray<Hero>,
   mousePositionRef: MutableRefObject<Position>,
 ) => {
   const lastDirectionChangeTimeRef = useRef<number>(0);
+
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
@@ -22,14 +24,19 @@ const useHeroAnimation = (
           ctx.fillStyle = colors.background;
           ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+          const now = Date.now();
           heroes.forEach((hero) => {
             hero.update(ctx, canvas, heroes);
 
             const distance = hero.getDistanceBetweenMouseAndHero(
               mousePositionRef.current,
             );
-            if (distance < mouseProximityRadius && (now - lastDirectionChangeTimeRef.current > debounceTime)) {
+            if (
+              distance < mouseProximityRadius &&
+              now - lastDirectionChangeTimeRef.current > debounceTime
+            ) {
               hero.changeDirection();
+              lastDirectionChangeTimeRef.current = now;
             }
           });
 
